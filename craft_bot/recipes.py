a43f2de16b4123ec7769b5f9dee070ebdd65f074
Denis Malkin <malkin.de@yandex.ru>
2017-03-17 00:31:41 +0300

    Begn implementing recipe tree

--- craft_bot/recipes.py
+++ craft_bot/recipes.py
@@ -2,19 +2,36 @@ from config import  Config
 import json
 import pprint
 import copy
+import collections
 
 class Recipes:
     def __init__(self, file_recipes):
         with open(file_recipes) as f:
-            self.recipes = json.load(f)
+            self.recipes = self.dict2counted(json.load(f))
+
+    @staticmethod
+    def dict2counted(d):
+        c = {}
+        for key, value in d.items():
+            c[key] = collections.Counter(value)
+        return c
+
+    def tree_expand(self, recipes):
+        for rec, ingredients in recipes.items():
+            for ingr in ingredients:
+                if ingr in recipes:
+                    ingr = (ingr, recipes[])
 
     def all(self):
         return self.recipes
 
-    def all_raw_recipes(self):
+    #returns resource list for which no any recipes
+    def all_raw_recipes(self, res_list):
         raw_res = []
-        for res in self.recipes:
+        for res in res_list:
             if res not in self.recipes:
+                raw_res.append(res)
+        return raw_res
 
     def find_recipe(self, resource_name):
         if resource_name not in self.recipes:
