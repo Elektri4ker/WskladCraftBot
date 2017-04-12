f0921d7d292ae76d46674e42bc34bad6bd94da2a
Denis Malkin <malkin.de@yandex.ru>
2017-04-13 00:07:18 +0300

    [WIP] ODM wrappers

---
+++ craft_bot/odm_classes.py
@@ -0,0 +1,17 @@
+from mongoengine import *
+import datetime
+
+class User(Document):
+    id = LongField(unique=True, primary_key=True, required=True)
+    username = StringField(required=True)
+    cw_username = StringField(required=True)
+
+    vk_link = StringField()
+
+    found_new_texts = DictField()
+    text_ids = ListField(LongField())
+
+    achievements_ids = ListField(LongField())
+
+class QuestDescriptor(Document):
+    id = LongField(unique=True, primary_key=True, required=True)
